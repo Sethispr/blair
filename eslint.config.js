144276ed2866691f2cc66f3412b3c419aab18947
seth <117851610+Sethispr@users.noreply.github.com>
2025-10-11 23:37:24 -0700

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import astro from 'eslint-plugin-astro';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...astro.configs.recommended,
+  {
+    ignores: [
+      '.github/workflows/**',
+    ],
+  },
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx', '**/*.astro'],
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
+      'no-console': 'warn',
+      'eqeqeq': ['error', 'always'],
+      'curly': ['error', 'all'],
+    },
+  },
+];
